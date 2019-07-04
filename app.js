40254bc23a3420bbbb3e3403a6767d39e70fc10f
Betty <s9654025@gmail.com>
2019-07-04 22:59:37 +0800

    Set up and connect to mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const methodOverride = requier('method-override')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useCreateIndex: true })
+const db = mongoose.connection
+
+db.on( 'error', () => {
+    console.log('db connect fail')
+})
+
+db.once('open', () => {
+    console.log('db connected!')
+})
 
 app.use('/', (req, res) => {
     res.render('index')
